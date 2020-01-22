b66dd33a8becacdd61135719d956606edb7d0701
Mikhail Gunin <gunka462@gmail.com>
2020-01-22 18:25:20 +0300

    Второе задание, первая реализация.

---
+++ lab2/lab2_nums.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    cout << "Enter X: " << endl;
+    cin >> x;
+
+    double eps;
+    cout << "Enter Epsilon: " << endl;
+    cin >> eps;
+
+    double a = x;
+    double s = x;
+    printf("a[0] = %lf\n", x);
+
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        double q = pow(x, 4)
+                /
+                   ((4 * i + 2) * (4 * i + 3) * (4 * i + 4) * (4 * i + 5));
+        a *= q;
+        s += a;
+
+        printf("a[%d] = %lf; s = %lf\n", i, a, s);
+
+        if (a <= eps)
+        {
+            cout << "\nN = " << i << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
