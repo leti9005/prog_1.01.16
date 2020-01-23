0aea7b1c503ba88b27672fdbba2765e3237d4c88
Mikhail Gunin <gunka462@gmail.com>
2020-01-23 15:01:22 +0300

    фиксы ошибок расчётов, дополнен репорт, создан докикс

--- lab1/lab1_iostream.cpp
+++ lab1/lab1_iostream.cpp
@@ -22,18 +22,14 @@ int main()
     cout << "Part 1: " << setw(16) << part1_1 << endl;
 
     // part1_2 = x * part1
-    double part1_2 = x * part1_1;
+    double part1_2 = x * part1_1 - 5.5;
     cout << "Part 2: " << setw(16) << part1_2 << endl;
 
-    // part1_3 = part1_2 - 5.5
-    double part1_3 = part1_2 - 5.5;
+    // part1_3 = x^2 * part1_2
+    double part1_3 = x * x * part1_2;
     cout << "Part 3: " << setw(16) << part1_3 << endl;
 
-    // part1_4 = x^2 * part1_3
-    double part1_4 = x * x * part1_3;
-    cout << "Part 4: " << setw(16) << part1_4 << endl;
-
-    cout << "First polynome result: " << part1_4 << "\n\n";
+    cout << "First polynome result: " << part1_3 << "\n\n";
 
     // x^3 (x^12 (-492.234x^2 + 434.432) + 0.2)
 
@@ -42,23 +38,19 @@ int main()
     double part2_1 = -492.234 * (x * x) + 434.432;
     cout << "Part 1: " << setw(16) << part2_1 << endl;
 
-    // part2_2 = x^12 * part2_1
-    double part2_2 = pow(x, 12) * part2_1;
+    // part2_2 = x^12 * part2_1 + 0.2
+    double part2_2 = pow(x, 12) * part2_1 + 0.2;
     cout << "Part 2: " << setw(16) << part2_2 << endl;
 
-    // part2_3 = part2_2 + 0.2
-    double part2_3 = part2_2 - 5.5;
+    // part2_3 = x^3 * part2_2
+    double part2_3 = pow(x, 3) * part2_2;
     cout << "Part 3: " << setw(16) << part2_3 << endl;
 
-    // part2_4 = x^3 * part2_3
-    double part2_4 = pow(x, 3) * part2_3;
-    cout << "Part 4: " << setw(16) << part2_4 << endl;
+    cout << "Second polynome result: " << part2_3 << "\n\n";
 
-    cout << "Second polynome result: " << part2_4 << "\n\n";
+    double division = part1_3 / part2_3;
 
-    double division = part1_4 / part2_4;
-
-    cout << "Final result: " << setprecision(5) << division;
+    cout << "Final result: " << setprecision(20) << fixed << division;
 
     return 0;
 }
